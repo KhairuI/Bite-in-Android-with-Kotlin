d95803c3d9dd2455c92f6fa0fa479d1ae2acee12
Khairul Islam <khairul4166@gmail.com>
2021-04-03 21:23:34 +0600

    Pass data between activity

--- app/src/main/java/com/example/bite_kotlin_form/MainActivity.kt
+++ app/src/main/java/com/example/bite_kotlin_form/MainActivity.kt
@@ -1,21 +1,53 @@
 package com.example.bite_kotlin_form
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.ByteArrayOutputStream
+import java.sql.Date
+import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var uri: Uri?=null
+    private var userTime:String?=null
+    private var userDate:String?=null
+    private var userBlood:String?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         pickImage.setOnClickListener(this)
+        pickDate.setOnClickListener(this)
+        pickTime.setOnClickListener(this)
+        submitButton.setOnClickListener(this)
+
+        val bloodList= resources.getStringArray(R.array.blood_group)
+        spinner.adapter= ArrayAdapter(this,android.R.layout.simple_list_item_1,bloodList)
+        spinner.onItemSelectedListener= object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, i: Int, p3: Long) {
+                userBlood= bloodList.get(i)
+              pickBloodText.text= bloodList.get(i)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
 
     }
 
@@ -25,7 +57,87 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.pickImage -> {
                 openDialogue()
             }
+            R.id.pickDate -> {
+                takeDate()
+            }
+
+            R.id.pickTime -> {
+                takeTime()
+            }
+
+            R.id.submitButton -> {
+               /* val intent= Intent(this,ProfileActivity::class.java)
+                intent.putExtra("name",pickName.text.toString())
+                startActivity(intent)*/
+                val userName= pickName.text.toString()
+                val userEmail= pickEmail.text.toString()
+                var userGender= ""
+                var userSkill= ""
+                when(radioGroup.checkedRadioButtonId){
+                    R.id.male ->{
+                        userGender= male.text.toString()
+                    }
+                    R.id.female ->{
+                    userGender= female.text.toString()
+                }
+
+                }
+
+                if(bangla.isChecked){
+                    userSkill+="Bangla "
+                }
+                if(english.isChecked){
+                    userSkill+="English "
+                }
+                if(arabic.isChecked){
+                    userSkill+="Arabic "
+                }
+                if(hindi.isChecked){
+                    userSkill+="Hindi "
+                }
+
+                val  student= Student(uri,userName,userEmail,userGender,userDate,userTime,
+                userBlood,userSkill)
+                val intent= Intent(this,ProfileActivity::class.java)
+                intent.putExtra("student",student)
+                startActivity(intent)
+            }
         }
+    }
+
+    private fun takeTime() {
+        val calender= Calendar.getInstance()
+        val hour= calender.get(Calendar.HOUR_OF_DAY)
+        val minute= calender.get(Calendar.MINUTE)
+
+        val timePickerDialog:TimePickerDialog=TimePickerDialog(this,
+            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+
+                userTime=""+hourOfDay+" : "+minute
+                pickTimeText.text= ""+hourOfDay+" : "+minute
+
+            },hour,minute,true)
+
+        timePickerDialog.show()
+
+
+    }
+
+    private fun takeDate() {
+        val calender= Calendar.getInstance()
+        val year= calender.get(Calendar.YEAR)
+        val month= calender.get(Calendar.MONTH)
+        val day= calender.get(Calendar.DAY_OF_MONTH)
+        val datePickerDialog: DatePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+            userDate= ""+dayOfMonth+"/"+monthOfYear+""+year
+            pickDateText.text= ""+dayOfMonth+"/"+monthOfYear+""+year
+        }, year, month+1, day)
+
+        datePickerDialog.show()
+
+
+
     }
 
     private fun openDialogue() {
@@ -71,13 +183,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         if (requestCode== 1 && resultCode== RESULT_OK){
 
+            uri= data?.data
             pickImage.setImageURI(data?.data)
+
         }
         else if(requestCode== 2 && resultCode== RESULT_OK){
             val bitmap= data?.extras?.get("data") as Bitmap
+            uri= getImageUriFromBitmap(this,bitmap)
             pickImage.setImageBitmap(bitmap)
+
+
         }
 
+    }
+
+    fun getImageUriFromBitmap(context: Context, bitmap: Bitmap): Uri{
+        val bytes = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(context.contentResolver, bitmap, "Title", null)
+        return Uri.parse(path.toString())
     }
 
 }
--- app/src/main/java/com/example/bite_kotlin_form/ProfileActivity.kt
+++ app/src/main/java/com/example/bite_kotlin_form/ProfileActivity.kt
@@ -2,10 +2,23 @@ package com.example.bite_kotlin_form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val student: Student?= intent.getParcelableExtra("student")
+        if (student != null){
+            profileImage.setImageURI(student.uri)
+            nameText.text= student.name
+            emailText.text= student.email
+            genderText.text= student.gender
+            dateText.text= student.date
+            timeText.text= student.time
+            bloodText.text= student.blood
+            skillText.text= student.skill
+        }
     }
 }
