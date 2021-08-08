d4fdd1cd9250e1d0fd47cfd332e7e9c4455a4c60
Khairul Islam <khairul4166@gmail.com>
2021-08-08 21:37:26 +0600

    map implement

--- app/src/main/java/com/example/bitegooglemap/fragments/ChooseFragment.kt
+++ app/src/main/java/com/example/bitegooglemap/fragments/ChooseFragment.kt
@@ -3,14 +3,14 @@ package com.example.bitegooglemap.fragments
 import android.location.Address
 import android.location.Geocoder
 import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.bitegooglemap.R
-import com.example.bitegooglemap.databinding.FragmentMapBinding
+import com.example.bitegooglemap.databinding.FragmentChooseBinding
 import com.example.bitegooglemap.utils.PermissionUtils
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -19,22 +19,20 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 import java.util.*
 
-@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-class MapFragment : Fragment(),OnMapReadyCallback,GoogleMap.OnMarkerClickListener {
 
-    private lateinit var binding: FragmentMapBinding
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class ChooseFragment : Fragment(),OnMapReadyCallback,GoogleMap.OnCameraMoveStartedListener,
+GoogleMap.OnCameraMoveListener,GoogleMap.OnCameraIdleListener{
+
+    private lateinit var binding: FragmentChooseBinding
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var lastLocation:Location
+    private lateinit var lastLocation: Location
     private var city=""
     private var currentLat=0.0
     private var currentLng=0.0
-    private var destinationLat=0.0
-    private var destinationLng=0.0
-    private lateinit var mMap:GoogleMap
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,70 +43,40 @@ class MapFragment : Fragment(),OnMapReadyCallback,GoogleMap.OnMarkerClickListene
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding= FragmentMapBinding.inflate(layoutInflater)
-        getValue()
+        binding= FragmentChooseBinding.inflate(layoutInflater)
         val mapFragment= childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        binding.btnMyLocation.setOnClickListener {
-            goToMayLocation()
-        }
-        binding.txtDestinationLocation.setOnClickListener {
-            goToChooseFragment()
+        binding.btnConfirmDestination.setOnClickListener {
+            if(binding.edtChooseDestination.text.toString().isNotEmpty()){
+                goToMapFragment()
+            }
         }
 
         return binding.root
     }
 
-    private fun getValue() {
-        val latLng= arguments?.getParcelable<LatLng>("latlng")
-        if(latLng==null){
-            Log.d("mymsg", "getValue: Empty")
-        }
-        else{
-            destinationLat= latLng.latitude
-            destinationLng= latLng.longitude
-        }
-        val name= arguments?.getString("city")
-        if(name.isNullOrBlank()){
-            binding.txtDestinationLocation.text="Choose Destination"
-        }
-        else{
-            binding.txtDestinationLocation.text=name
-        }
-    }
-
-    private fun goToChooseFragment() {
+    private fun goToMapFragment() {
+        val bundle= Bundle()
+        val latLng= LatLng(currentLat,currentLng)
+        bundle.putParcelable("latlng",latLng)
+        bundle.putString("city",city)
+        val mapFragment= MapFragment()
+        mapFragment.arguments= bundle
         activity?.supportFragmentManager?.beginTransaction()
             ?.setCustomAnimations(
                 R.anim.slide_in_right_animation,
                 R.anim.slide_out_left_animation,
                 R.anim.slide_in_left_animation,
                 R.anim.slide_out_right_animation
-            )?.replace(R.id.fragment_container,ChooseFragment())?.addToBackStack(null)?.commit()
-    }
-
-    private fun goToMayLocation() {
-        if(PermissionUtils.checkPermission(requireContext())){
-            fusedLocationProviderClient.lastLocation.addOnCompleteListener {
-                val location= it.result
-                if(location != null){
-                    lastLocation= location
-                    currentLat= location.latitude
-                    currentLng= location.longitude
-                    val currentLatLang= LatLng(lastLocation.latitude,lastLocation.longitude)
-                    addMarker(currentLatLang)
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLang,15f))
-                }
-            }
-        }
-
+            )?.disallowAddToBackStack()?.replace(R.id.fragment_container,mapFragment)?.commit()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap= googleMap
         mMap.uiSettings.isZoomControlsEnabled= false
         mMap.uiSettings.isMyLocationButtonEnabled= false
-        mMap.setOnMarkerClickListener(this)
+        mMap.setOnCameraIdleListener(this)
+        mMap.setOnCameraMoveStartedListener(this)
         setMap()
     }
 
@@ -122,27 +90,42 @@ class MapFragment : Fragment(),OnMapReadyCallback,GoogleMap.OnMarkerClickListene
                     currentLat= location.latitude
                     currentLng= location.longitude
                     val currentLatLang= LatLng(lastLocation.latitude,lastLocation.longitude)
-                    addMarker(currentLatLang)
+                    val address= getAddress(lastLocation)
+                    city= address[0].getAddressLine(0).toString()
+                    binding.edtChooseDestination.setText(city)
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLang,15f))
                 }
             }
         }
-    }
-
-    private fun addMarker(currentLatLang: LatLng) {
-        val address= getAddress(lastLocation)
-        city= address[0].getAddressLine(0).toString()
-        binding.txtSelfLocation.text= city
-        val markerOption= MarkerOptions().position(currentLatLang).title(city)
-        mMap.addMarker(markerOption)
-
     }
     private fun getAddress(location: Location): MutableList<Address> {
         val geocoder = Geocoder(context, Locale.getDefault())
         return geocoder.getFromLocation(location.latitude, location.longitude, 5)
     }
 
-    override fun onMarkerClick(p0: Marker)= false
 
+    override fun onCameraMoveStarted(reason: Int) {
+        if(reason== GoogleMap.OnCameraMoveStartedListener.REASON_GESTURE){
+            mMap.setOnCameraMoveStartedListener(this)
+        }
+    }
 
+    override fun onCameraMove() {
+    }
+
+    override fun onCameraIdle() {
+        val cameraPosition= mMap.cameraPosition
+        val location= Location(LocationManager.GPS_PROVIDER)
+        location.apply {
+            latitude= cameraPosition.target.latitude
+            longitude= cameraPosition.target.longitude
+        }
+        currentLat= location.latitude
+        currentLng= location.longitude
+        val address= getAddress(location)
+        city= address[0].getAddressLine(0).toString()
+        binding.edtChooseDestination.setText(city)
+        mMap.setOnCameraMoveListener(this)
+
+    }
 }
